ccce3cab9541a45caa9e561da3bc612e2752ad25
Hugo <hugo_moreira@hotmail.com>
2023-10-22 22:58:38 +0100

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/hugormm.github.io/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/pokedex/" : "/"
 })
